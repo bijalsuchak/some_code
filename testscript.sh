c691fde9902926d450ca74b6118445dcf3b5dd98
Bijal Suchak <bijal@Bijals-MBP.fritz.box>
2022-03-23 10:25:49 +0000

    added

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,6 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "completed"
-echo "This TEXT has been added"
+echo "Hello Bijal"
 echo "Some more TEXT has been added"
 echo "Hello World"
